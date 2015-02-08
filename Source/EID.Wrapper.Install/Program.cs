9e56b8f1b50efd293d93993fe23a1f2bb89ca94f
Jesper De Temmerman <jesper@toron.be>
2015-02-08 21:31:23 +0100

    Renamed 'install' to 'register' to better describe solution and added some more output and error checking

--- Source/EID.Wrapper.Install/Program.cs
+++ Source/EID.Wrapper.Install/Program.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Security.Principal;
 
-namespace EID.Wrapper.Install
+namespace EID.Wrapper.Register
 {
     class Program
     {
@@ -12,6 +12,8 @@ namespace EID.Wrapper.Install
             if(!HasAdministratorRights())
             {
                 Console.WriteLine("This tool needs administrator privileges to copy and register files. Please run this tool as Administrator");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
                 return;
             }
 
@@ -20,10 +22,10 @@ namespace EID.Wrapper.Install
 
             var regasm = Path.Combine(System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory(), "regasm.exe");
 
-            Console.WriteLine("Copying EID.Wrapper.dll to " + destination);
+            Console.WriteLine("Copying EID.Wrapper.dll to " + destination + "...");
             File.Copy("EID.Wrapper.dll", destination, true);
 
-            Console.WriteLine("Registering EID.Wrapper.dll in " + destination + " as codebase and generating tlb with regasm");
+            Console.WriteLine("Registering EID.Wrapper.dll in " + destination + " as codebase and generating tlb with regasm...");
             Console.WriteLine("--------------------------------------------------------------------------------");
 
             var info = new ProcessStartInfo(regasm, string.Format("{0} /tlb:{1} /codebase", destination, tlb));
@@ -32,7 +34,11 @@ namespace EID.Wrapper.Install
             proc.WaitForExit();
 
             Console.WriteLine("--------------------------------------------------------------------------------");
-            Console.WriteLine("SUCCESS: Wrapper registered successfully");
+            if (proc.ExitCode == 0)
+                Console.WriteLine("SUCCESS: Wrapper registered successfully");
+            else
+                Console.WriteLine("WARNING: The DLL was copied but regasm returned an unexpected return value. Check to make sure that the call to regasm succeeded.");
+
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
--- Source/EID.Wrapper.Install/Properties/AssemblyInfo.cs
+++ Source/EID.Wrapper.Install/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EID.Wrapper.Install")]
+[assembly: AssemblyTitle("EID.Wrapper.Register")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EID.Wrapper.Install")]
+[assembly: AssemblyProduct("EID.Wrapper.Register")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
